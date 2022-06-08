afa92bb8420d9a494b392a3294faa001425e78b9
ammwangi <elijah.mwangi@kamsware.com>
2022-06-08 15:13:30 +0300

    Add project files.

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+        public string EmailAddress { get; set; }
+        public string FisrtName { get; set; }
+
+        public string FullName 
+        {
+            get { return LastName + "," + FisrtName; }
+        }
+        private string _lastName;
+
+        public string LastName { 
+            get 
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+
+
+        }
+    }
+}
